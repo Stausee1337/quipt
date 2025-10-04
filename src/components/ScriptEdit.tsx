a5cc6db9e80be154af8f6989b09bdabdc237775f
Simon König <stausee1337@pop-os.localdomain>
2025-10-04 23:43:21 +0200

    ScriptEdit: adding editing capabilities

--- src/components/ScriptEdit.tsx
+++ src/components/ScriptEdit.tsx
@@ -1,37 +1,113 @@
-import { JSX, createEffect, createMemo, onMount, untrack, useContext } from 'solid-js';
+import { JSX, createEffect, getOwner, onCleanup, onMount, runWithOwner, useContext } from 'solid-js';
 import { EditorView, minimalSetup } from 'codemirror';
-import { EditorState } from '@codemirror/state';
 import { markdown } from '@codemirror/lang-markdown';
-import { Script } from '../backend';
-import { renderCuePair } from './TextCueView';
+import { placeholder } from "@codemirror/view";
+import { HighlightStyle, syntaxHighlighting } from "@codemirror/language";
+import { tags } from "@lezer/highlight";
+import { Script, TextCue, TextCuePair } from '../backend';
+import { renderCue } from './TextCueView';
 import { ScriptContextObj } from '../script';
 import { DivisionInfoView } from './DivisionInfoView';
 import { useNavigate, useParams } from '@solidjs/router';
+import { DialogManager } from '../dialog';
 
-const myTheme = EditorView.theme({
-  // ".cm-content": {
-  //   caretColor: "red",  // caret (cursor) color
-  // },
-  // "&.cm-focused .cm-cursor": {
-  //   borderLeftColor: "red"  // caret visible when focused
-  // },
-  // "&.cm-focused .cm-selectionBackground, ::selection": {
-  //   backgroundColor: "rgba(0, 128, 255, 0.3)"  // selection color
-  // }
-}, {dark: true}) // set dark: true if this is a dark theme
+const myTheme = EditorView.theme({}, {dark: true})
 
-function Editor(): JSX.Element {
+const customMarkdownStyle = HighlightStyle.define([
+    // Color for the Markdown formatting markers (e.g., **, _, #)
+    { tag: tags.processingInstruction, color: "#e3e3e3" },
+    { tag: tags.meta, color: "#ff6b81" },
+    { tag: tags.strong, fontWeight: "bold" },
+    { tag: tags.emphasis, fontStyle: "italic" },
+    { tag: tags.strikethrough, textDecoration: "line-through" },
+]);
+
+function Editor(props: {
+    content: string,
+    onChange?: (content: string) => void
+}): JSX.Element {
     const view = new EditorView({
+        doc: props.content,
         extensions: [
             myTheme,
             minimalSetup,
+            EditorView.lineWrapping,
+            placeholder("Text einfügen"),
             markdown(),
-            EditorState.transactionFilter.of(tr => {
-                return tr.newDoc.lines > 1 ? [] : [tr]
+            syntaxHighlighting(customMarkdownStyle),
+            EditorView.updateListener.of(update => {
+                if (update.docChanged)
+                    props.onChange?.(update.state.doc.toString());
             })
         ],
     });
     return view.dom;
+}
+
+function EditableTextCue(
+    props: {
+        textCue: Readonly<TextCue>|null,
+        type: "request"|"response"
+    }
+): JSX.Element {
+    const cueComponent = renderCue(props.textCue, props.type);
+    let revoker: (() => void)|undefined;
+    let owner = getOwner()!;
+
+    function onClick() {
+        if (revoker === undefined) {
+            cueComponent.cueElement.classList.add('editing');
+            revoker = cueComponent.injectContent(
+                runWithOwner(owner, () =>
+                    <>
+                        <Editor content={props.textCue?.text ?? ''} onChange={console.log}/>
+                        <div class="edit-commit-container"></div>
+                    </>
+                )
+            );
+        }
+    }
+
+    onMount(() => {
+        cueComponent.cueElement.addEventListener('click', onClick);
+    });
+
+    onCleanup(() => {
+        cueComponent.cueElement.removeEventListener('click', onClick);
+    });
+
+    return cueComponent;
+}
+
+function GapInjectHandle(
+    props: { static?: boolean }
+): JSX.Element {
+    return (
+        <div class="gap-inject-handle" classList={{ static: props.static }}>
+            {
+                props.static ? null : <i class="bi bi-plus-circle"/>
+            }
+        </div>
+    );
+}
+
+function renderCuePair(textCuePair: Readonly<TextCuePair>): JSX.Element {
+    return (
+        <>
+            <EditableTextCue textCue={textCuePair.request} type="request"/>
+            <GapInjectHandle static/>
+            <EditableTextCue textCue={textCuePair.response} type="response"/>
+            <GapInjectHandle/>
+        </>
+    )
+}
+
+function CreateCueDialog() {
+    return (
+        <>
+            <h2>Einsatz hinzufügen</h2>
+        </>
+    );
 }
 
 function ScriptCueView(
@@ -47,6 +123,17 @@ function ScriptCueView(
         document.title = `${props.script.name} - Quipt`
     })
 
+    function onClick(event: MouseEvent) {
+        const path = event.composedPath();
+        const handle = path.find(
+            t => (t instanceof HTMLDivElement)
+            && t.classList.contains('gap-inject-handle')) as HTMLDivElement|undefined;
+        if (handle === undefined) return;
+        DialogManager.openDialog(
+            CreateCueDialog
+        );
+    }
+
     return (
         <div class="desktop-view">
             <h2 class="script-title">{ props.script.name }</h2>
@@ -59,9 +146,10 @@ function ScriptCueView(
                 {
                     props.script.divisions.map(division => {
                         return (
-                            <div class="script-divsion">
+                            <div class="script-divsion" onClick={onClick}>
                                 <h2>{ division.name }</h2>
                                 <DivisionInfoView division={division}/>
+                                <GapInjectHandle/>
                                 { division.textCues.map(renderCuePair) }
                             </div>
                         );
